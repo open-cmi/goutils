1d9c6aee16a31c946b731c48b0279e64e917976f
open-cmi <danielzhao2029@gmail.com>
2021-04-03 23:19:30 +0800

    fix: mongo database

--- database/dbmongo/mongo.go
+++ database/dbmongo/mongo.go
@@ -4,43 +4,34 @@ import (
 	"context"
 	"strconv"
 
-	"github.com/open-cmi/goutils/config"
+	"github.com/open-cmi/goutils/database"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-var MongoClient *mongo.Client
-
 // MongoInit db use mongo
-func MongoInit() error {
-	host := config.Conf.GetStringMap("model")["host"].(string)
-	port := config.Conf.GetStringMap("model")["port"].(int)
-	user := config.Conf.GetStringMap("model")["user"].(string)
-	password := config.Conf.GetStringMap("model")["password"].(string)
-	dbname := config.Conf.GetStringMap("model")["dbname"].(string)
+func MongoInit(conf *database.Config) (client *mongo.Client, err error) {
+	host := conf.Host
+	port := conf.Port
+	user := conf.User
+	password := conf.Password
+	dbname := conf.Database
 
 	uri := "mongodb://" + host + ":" + strconv.Itoa(port)
 	if user != "" {
 		uri = "mongodb://" + user + ":" + password + "@" + host + ":" + strconv.Itoa(port) + "/" + dbname
 	}
 
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	if err != nil {
-		return err
+		return nil, err
 	}
 	err = client.Ping(context.TODO(), readpref.Primary())
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	MongoClient = client
-	return nil
-}
-
-// MongoFini close connection
-func MongoFini() {
-	MongoClient.Disconnect(context.TODO())
-	return
+	return client, nil
 }
